8849776ec7056cab1de67cc44625a5e209a8e378
tren03 <82367813+tren03@users.noreply.github.com>
2023-07-27 10:45:46 +0530

    Create page_rep_lru(to-be-fixed).c

---
+++ page_rep_lru(to-be-fixed).c
@@ -0,0 +1,74 @@
+#include<stdio.h>
+#include <stdbool.h>
+bool isPresent(int fn, int a, int frame[]) {
+    for (int i = 0; i < fn; i++) {
+        if (frame[i] == a) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    int n,f,fn,fifo;//fifo stores the index if the element to be replaced
+    int arr[100];
+    int frame[10];
+    printf("enter page numbers : ");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        printf("\nenter page number of page %d ",i+1);
+        scanf("%d",&arr[i]);        
+    }
+
+    
+    printf("\nenter frame number");
+    scanf("%d",&fn);
+    int lru=0;
+    int miss=0;
+    int cur=0,hit=0;
+    for(int i=0;i<n;i++)
+    {
+        if(cur!=fn)
+        {                    
+            frame[cur]=arr[i];
+            cur++;   
+            miss++;        
+        }
+        else
+        {
+            if(isPresent(fn,arr[i],frame))
+            {
+                hit++;
+            }
+            else
+            {
+                for(int k=i-1;k>=0;k--)
+                {
+                    for(int p=0;p<fn;p++)
+                    {
+                        if(arr[k]==frame[p])
+                        {
+                            lru=p;
+                        }
+
+                    }                
+                  
+                }
+                frame[lru]=arr[i];
+                miss++;
+                // frame[fifo]=arr[i];
+                // fifo=(fifo+1)%fn;
+                // miss++;
+            }
+        }
+
+    }
+    printf("\nhits : %d",hit);
+    printf("\nmiss : %d",miss);
+    
+    
+    
+
+}
